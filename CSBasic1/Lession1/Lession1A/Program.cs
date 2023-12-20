f8a2c8a36e5b490ef34b2170f05d51bea57901d9
Dũng Việt <thanhtruckoy2007@gmail.com>
2023-12-21 00:13:52 +0700

    init lession1

---
+++ CSBasic1/Lession1/Lession1A/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello T3H"); // hiển thị lên màn hình
+Console.WriteLine("Hello T3H1");
+Console.WriteLine("Hello T3H2");
+Console.WriteLine("Hello T3H3");
+Console.WriteLine("Hello T3H4");
+Console.WriteLine("Hello T3H5"); 
+
+Console.ReadKey(); // dừng màn hình đen chạy lại
---
+++ CSBasic1/Lession1/Lession1B/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Viet Nam!");
+Console.WriteLine("Hello,Cau Giay");
+
+Console.ReadKey(); // dừng màn hình đen chạy lại
